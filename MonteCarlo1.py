104037b67d6982bcd9272b929b9207d83e67793f
MaiaMarcioD <98567047+MaiaMarcioD@users.noreply.github.com>
2023-01-26 11:53:19 -0300

    Update MonteCarlo1.py

--- MonteCarlo1.py
+++ MonteCarlo1.py
@@ -8,6 +8,5 @@ from MonteCarlo import MonteCarlo
 if __name__ == "__main__":
 
     MonteCarlo(10, 7, 9822) 
-    # Dois primeiros parametros da função condiz com as dimensões   
-
+    # The first two parameters of the function match the dimensions
     
